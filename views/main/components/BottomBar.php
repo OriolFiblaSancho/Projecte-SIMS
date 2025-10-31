b54dd3d7cb4fffae22ac35a4bb8620795768b86c
oriolfiblasancho <oriolfibla@iesmontsia.org>
2025-10-31 01:19:17 +0100

    refactor: redo of main.php to divide the code on different components

---
+++ views/main/components/BottomBar.php
@@ -0,0 +1,33 @@
+<?php
+// Bottom bar component with selected car info and scan button
+?>
+<div id="bottomBar" class="fixed inset-x-0 bottom-0 z-50 flex flex-col">
+  <div id="selectedCarInfo" class="hidden">
+    <p id="car_street" class="text-neutral-700">c/Street X,39</p>
+    <p id="car_plate" class="text-neutral-700">0340 GDH</p>
+
+    <div id="car_battery">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" class="size-8">
+        <path d="M4.5 9.75a.75.75 0 0 0-.75.75V15c0 .414.336.75.75.75h6.75A.75.75 0 0 0 12 15v-4.5a.75.75 0 0 0-.75-.75H4.5Z" />
+        <path fill-rule="evenodd" d="M3.75 6.75a3 3 0 0 0-3 3v6a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-.037c.856-.174 1.5-.93 1.5-1.838v-2.25c0-.907-.644-1.664-1.5-1.837V9.75a3 3 0 0 0-3-3h-15Zm15 1.5a1.5 1.5 0 0 1 1.5 1.5v6a1.5 1.5 0 0 1-1.5 1.5h-15a1.5 1.5 0 0 1-1.5-1.5v-6a1.5 1.5 0 0 1 1.5-1.5h15Z" clip-rule="evenodd" />
+      </svg>
+      <p>145 Km</p>
+    </div>
+
+    <button id="reserve_button" class="bg-[#7E3FBC] text-white rounded flex px-8 py-4 font-bold">Reserve</button>
+  </div>
+
+  <a href="/views/scan/pages/scan.html">
+    <button id="scanBtn" class="scan-btn md:hidden" aria-label="Scan QR">
+      <span class="scan-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 md:hidden">
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z" />
+          <path stroke-linecap="round" stroke-linejoin="round"
+            d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z" />
+        </svg>
+      </span>
+      <span class="scan-text">Scan QR</span>
+    </button>
+  </a>
+</div>
